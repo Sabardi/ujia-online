72f8168fc5545aeaa93fa747feaa8ee2cd604cd4
Sabardi <Sabardibahari@gmail.com>
2024-11-16 02:34:37 +0700

    feat: model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [
+        'id',
+    ];
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function questions()
+    {
+        return $this->hasMany(CourseQuestion::class, 'course_id', 'id');
+    }
+
+    public function students()
+    {
+        return $this->belongsToMany(User::class, 'course_students', 'user_id', 'course_id');
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function course()
+    {
+        return $this->belongsToMany(Course::class, 'course_students', 'user_id', 'course_id');
+    }
+
 }
